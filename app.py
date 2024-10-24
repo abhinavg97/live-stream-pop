6818480e2e0f1339f03f00e335c60dbcaa268386
abhinavg97 <gabhinav001@gmail.com>
2024-10-24 18:07:38 -0400

    add metrics for pop

--- app.py
+++ app.py
@@ -8,20 +8,27 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 cache = {}
+cache_hits, cache_misses = 0, 0
 
 
 @app.route('/<path:filename>')
 def stream_hls(filename):
     """Serve HLS playlist and segments."""
+    global cache_hits, cache_misses
 
     if filename in cache:
+        cache_hits += 1
         # cache the file corresponding to {filename} in in-memory cache
-        print(f"cache hit for {filename}")
+        print(f"cache hits: {cache_hits}, cache_misses: {cache_misses},"
+              f" percentage of hits: {(cache_hits * 100) / (cache_hits + cache_misses)}")
         return cache[filename]
 
+    cache_misses += 1
     r = requests.get(f'http://localhost:8001/{filename}')
     cache[filename] = Response(r.content, status=r.status_code, content_type=r.headers['Content-Type'])
-    print(f"length of cache now is {len(cache)}")
+
+    print(f"cache hits: {cache_hits}, cache_misses: {cache_misses},"
+          f" percentage of hits: {(cache_hits*100)/(cache_hits + cache_misses)}")
     return cache[filename]
 
 
@@ -34,6 +41,7 @@ def get_chunk():
     # (time wise bucketing to respond to the clients with HLS file)
     r = requests.get('http://localhost:8001/stream')
     return Response(r.content, status=r.status_code, content_type=r.headers['Content-Type'])
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8002)
