7a5c0ad1daea732e47389d91b29a38dc4fea621c
Ajit Sharma Kasturi <ak5055@columbia.edu>
2024-12-12 17:01:30 -0500

    Pop changes

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
 import os
+import random
 import time
-from flask import Flask, send_from_directory, Response, render_template
+from prometheus_client import Counter, generate_latest, Gauge
+from flask import Flask, send_from_directory, Response, render_template, redirect, url_for
 from flask_cors import CORS  # Import Flask-CORS
 import requests
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app, resources={r"/*": {"origins": "*"}})  # Explicitly allow all origins
+# Create a counter to track requests
+REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests (RPS)', ['method', 'endpoint'])
 
 cache = {}
 cache_hits, cache_misses = 0, 0
 
 
+def random_sleep(min_seconds=0, max_seconds=2):
+    """
+    Pauses execution for a random duration between min_seconds and max_seconds.
+
+    Args:
+        min_seconds (float): Minimum sleep duration in seconds (default is 0).
+        max_seconds (float): Maximum sleep duration in seconds (default is 2).
+    """
+    sleep_duration = random.uniform(min_seconds, max_seconds)
+    print(f"Sleeping for {sleep_duration:.2f} seconds...")
+    time.sleep(sleep_duration)
+    print("Done sleeping!")
+
+
 @app.route('/<path:filename>')
 def stream_hls(filename):
     """Serve HLS playlist and segments."""
+    REQUEST_COUNT.labels(method='GET', endpoint='/chunk_file').inc()  # Increment request counter
     global cache_hits, cache_misses
+    random_sleep()
 
     if filename in cache:
         cache_hits += 1
@@ -34,6 +54,7 @@ def stream_hls(filename):
 
 @app.route('/stream')
 def get_chunk():
+    REQUEST_COUNT.labels(method='GET', endpoint='/stream').inc()  # Increment request counter
     """Serve the dynamically updated HLS master playlist with looping."""
 
     # we can avoid hitting the origin for the HLS files by caching th HLS file response in the PoP server
@@ -43,5 +64,17 @@ def get_chunk():
     return Response(r.content, status=r.status_code, content_type=r.headers['Content-Type'])
 
 
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    # Expose Prometheus metrics
+    return Response(generate_latest(), mimetype="text/plain")
+
+
+@app.route('/')
+def home():
+    # Redirect to the /stream endpoint
+    return "Health check succeeded !"
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8002)
+    app.run(debug=True, port=8002, threaded=True)
